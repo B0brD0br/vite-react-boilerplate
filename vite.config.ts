633583637cd0119546f2a5807bb09cc5221517e2
B0brD0br <vazhnicky+github@gmail.com>
2023-02-18 21:28:11 +0000

    Install Vitest coverage

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,22 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   test: {
     globals: true,
+    reporters: ['verbose'],
+    coverage: {
+      all: true,
+      reporter: ['text', 'html', 'lcov'],
+      include: ['**/src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        '**/src/main.{js,jsx,ts,tsx}',
+        '**/*.types.{ts,tsx}',
+        '**/*.test.{js,jsx,ts,tsx}',
+        '**/src/vite-env*',
+      ],
+      statements: 0,
+      branches: 0,
+      functions: 0,
+      lines: 0,
+    },
   },
   plugins: [react()],
 })
