85bad49bcb06fc53816fa6b8a6577cdeb3e01fa2
B0brD0br <vazhnicky+github@gmail.com>
2023-02-25 14:12:44 +0000

    Install RTL
    * RTL
    * ESLint plugin for RTL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   test: {
     globals: true,
+    environment: 'jsdom',
     reporters: ['verbose'],
+    setupFiles: './src/test/setup.ts',
     coverage: {
       all: true,
       reporter: ['text', 'html', 'lcov'],
